4c956fb6f75a4769fd3802afca736e92055d9c11
Eric Chen <158211571+SacredFall@users.noreply.github.com>
2024-12-09 17:49:27 -0500

    a

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"], // Adjust the paths according to your project structure
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
